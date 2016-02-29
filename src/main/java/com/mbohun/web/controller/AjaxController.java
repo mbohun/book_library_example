7c685cc2023fa25845739955f507122fd5793130
mbohun <martin.bohun@gmail.com>
2016-03-01 08:36:02 +1100

    init version

---
+++ src/main/java/com/mbohun/web/controller/AjaxController.java
@@ -0,0 +1,9 @@
+package com.mbohun.web.controller;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
